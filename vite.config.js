2cc13fa23c509a1b315697f395597a86378c6b7c
Mas Abdi <hub.halloabdi@gmail.com>
2026-01-02 22:54:30 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  // Optimasi build untuk production
-  build: {
-    outDir: 'dist',
-    sourcemap: false, // Matikan sourcemap untuk hemat size & keamanan
-    minify: 'terser',
-  }
 })
